f47c36c815ddec30f58f517b27fd7be347d593b9
chandu11288@gmail.com <chandu11288@gmail.com>
2018-01-18 00:56:53 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,10 +1,12 @@
 def display_board(board)
- puts"#{board[0]} | #{board[1]} | #{board[2]}"
- puts "-----------"
- puts"#{board[3]} | #{board[4]} | #{board[5]}"
- puts "-----------"
- puts "#{board[6]} | #{board[7]} | #{board[8]}"
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts"-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts"-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
+board =["X","","","","X","","X","X","X"]
 
 def valid_move?(board,index)
  if index < 9 && index > -1 && board[index] != 'X' && board[index] != 'O'
