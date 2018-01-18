407c1de594354d744fa95a68fd50c18cc391b1d5
chandu11288@gmail.com <chandu11288@gmail.com>
2018-01-18 00:53:43 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,19 @@
+def display_board(board)
+ puts"#{board[0]} | #{board[1]} | #{board[2]}"
+ puts "-----------"
+ puts"#{board[3]} | #{board[4]} | #{board[5]}"
+ puts "-----------"
+ puts "#{board[6]} | #{board[7]} | #{board[8]}"
+end
+
+def valid_move?(board,index)
+ if index < 9 && index > -1 && board[index] != 'X' && board[index] != 'O'
+  return true
+ else
+  return false
+ end
+end 
+
+def input_to_index(num)
+  num.to_i - 1
+end
